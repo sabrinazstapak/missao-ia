aadef4fe1090eb265a3388b7163abcded8675c67
sabrinazstapak <163079437+sabrinazstapak@users.noreply.github.com>
2024-08-15 22:53:40 -0300

    alternativa

--- script.js
+++ script.js
@@ -48,5 +48,12 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+function mostraAlternativa() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 mostraPergunta();
